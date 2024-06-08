281817d6fded79ea37c37be584513b1e5403c98e
viktor <anfly0@gmail.com>
2024-06-08 16:21:34 +0200

    Removes unused function

--- cmd/robo-server/middleware.go
+++ cmd/robo-server/middleware.go
@@ -53,9 +53,3 @@ func ContentHeader(next http.Handler) http.Handler {
 		next.ServeHTTP(w, r)
 	})
 }
-
-func TrailingSlash(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		next.ServeHTTP(w, r)
-	})
-}
